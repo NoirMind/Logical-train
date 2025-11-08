453f5a8b7ff15e1d66a9ef0e4d62f3953d61aa88
AslanSoft <beggyplayy@gmail.com>
2025-11-08 17:18:20 +0500

    Logical Train

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
-const div = document.querySelector("#boxes");
-div.addEventListener("mouseover", hover)
-div.addEventListener("mouseout", hoverOut)
+const plus = document.querySelector('#plus')
+const minus = document.querySelector('#minus')
+const div = document.querySelector('#schots')
+let counter = 0
+plus.addEventListener('click', qoshish)
+minus.addEventListener('click', ayirish)
+
+function qoshish(){
+    counter++
+    const item = document.createElement('p')
+    item.classList.add('son')
+    div.appendChild(item)
+    item.textContent = counter
+}
+
+function ayirish(){
+    const deleteCount = document.createElement("h1")
+    deleteCount.textContent = "Tozalandi"
+    div.appendChild(deleteCount)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inpt = document.querySelector("#ipt");
+// const div = document.querySelector('#ota')
+// function yechim(){
+//     const item = document.createElement('li')
+//     item.classList.add("ul-item")
+//     item.textContent = inpt.value.trim()
+//     ota.appendChild(item)
+// }
+
+// div.addEventListener("mouseover", hover)
+// div.addEventListener("mouseout", hoverOut)
 
 // 2) function hover(){
 //     div.style.transform = 'scale(1.1)'; 
