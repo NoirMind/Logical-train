256776dfb597e448ee6a8b20fab289e4e5739c0a
AslanSoft <beggyplayy@gmail.com>
2025-11-08 16:44:23 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const div = document.querySelector("#boxes");
+div.addEventListener("mouseover", hover)
+div.addEventListener("mouseout", hoverOut)
+
+// 2) function hover(){
+//     div.style.transform = 'scale(1.1)'; 
+//         div.style.transition = 'transform 0.3s ease'
+// }
+
+// function hoverOut(){
+//     div.style.transform = 'scale(0.9)'; 
+//         div.style.transition = 'transform 0.3s ease'
+// }
+
+// 1) let counter = 1
+
+// btn.addEventListener('click', qiymat)
+
+// function qiymat(){
+//     if(counter === 1){
+//         counter++
+//         console.log(`Son oshdi, ${counter}`);
+//     }
+// }
